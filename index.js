a82be57778468e1c07045d84a37c32a746c448f2
Alvin Hart <pageking123@gmail.com>
2022-11-18 08:56:10 +0900

    Update files

--- index.js
+++ index.js
@@ -12,22 +12,22 @@ import {
 } from "./todo";
 
 
-TaskCreate.setDate()
-TaskCreate.setName("Pay the taxes!")
-TaskCreate.setPriority(3)
-TaskCreate.setTag("Work")
-TaskActions.makeNewTask()
+// TaskCreate.setDate()
+// TaskCreate.setName("Pay the taxes!")
+// TaskCreate.setPriority(3)
+// TaskCreate.setTag("Work")
+// TaskActions.makeNewTask()
 
-SeeTasks.seeFolder("Private")
-SeeTasks.seeInbox()
-// console.log(taskViewer.currentTasks.length);
-Editor.changeFolder("Pay the taxes!", "Private")
-SeeTasks.seeFolder("Private")
-Editor.editTaskDate("Pay the taxes!", 16, 4, 0, "pm")
-TagActions.editATag("Work", "Thing", "pink")
-FolderActions.createNewFolder("Alvin", "green")
-SeeTasks.seeFolder("Alvin")
-SeeTasks.seeUpcoming()
-SeeTasks.seeInbox()
-TaskActions.completeATask("Pay bills")
-console.log(folderManager.completed);
+// SeeTasks.seeFolder("Private")
+// SeeTasks.seeInbox()
+// // console.log(taskViewer.currentTasks.length);
+// Editor.changeFolder("Pay the taxes!", "Private")
+// SeeTasks.seeFolder("Private")
+// Editor.editTaskDate("Pay the taxes!", 16, 4, 0, "pm")
+// TagActions.editATag("Work", "Thing", "pink")
+// FolderActions.createNewFolder("Alvin", "green")
+// SeeTasks.seeFolder("Alvin")
+// SeeTasks.seeUpcoming()
+// SeeTasks.seeInbox()
+// TaskActions.completeATask("Pay bills")
+// console.log(folderManager.completed);
